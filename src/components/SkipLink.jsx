2237a24a55c5dd62aca629ebb1935564ca127c3a
PraZ <sachabouillez@gmail.com>
2020-11-09 00:10:54 +0100

    :fire: remove skiplink for now

--- src/components/SkipLink.jsx
+++
@@ -1,9 +0,0 @@
-import { SkipNavLink } from '@chakra-ui/skip-nav'
-
-const SkipLink = () => (
-  <SkipNavLink zIndex={2} top='auto' left='auto'>
-    Skip to content
-  </SkipNavLink>
-)
-
-export default SkipLink
