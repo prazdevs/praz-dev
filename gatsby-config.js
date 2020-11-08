d235e7a92706033ce2d227865cad331b194b4114
PraZ <sachabouillez@gmail.com>
2020-11-09 00:11:56 +0100

    :mag: update icon and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,14 +51,11 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#303030`,
+        theme_color: `#ff8787`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `static/images/icon.png`
       }
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ]
 }
