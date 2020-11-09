fba27b465cb6c3387a2948f57f3f9cef6890df32
PraZ <sachabouillez@gmail.com>
2020-11-09 01:38:44 +0100

    :rotating_light: import graphql properly in index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
 import { Box, Flex, Heading, Link, Stack, Text } from '@chakra-ui/core'
-import { Link as GatsbyLink } from 'gatsby'
+import { Link as GatsbyLink, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
 import Layout from '../components/Layout'
